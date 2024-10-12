ba66c8493abdcb1cfe5c43955b61e7355822e2b6
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2024-10-12 20:33:26 +0500

    refactor: refactor goodbye function with adding a process with exit code

--- src/utils/goodbyeUser.js
+++ src/utils/goodbyeUser.js
@@ -1,3 +1,6 @@
+import { exitCode } from "../constants/constants.js";
+
 export const goodbyeUser = (userName) => {
   console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+  process.exit(exitCode);
 };
