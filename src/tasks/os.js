f2e75af772bb822897dbac6bce3afd1797164820
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2024-10-13 15:22:33 +0500

    feat: add EOL and default cases in os task

--- src/tasks/os.js
+++ src/tasks/os.js
@@ -4,30 +4,7 @@ export const os = (parameter) => {
   import("node:os").then((osModule) => {
     switch (parameter) {
       case "--EOL":
-        console.log(JSON.stringify(osModule.EOL));
-        break;
-
-      case "--cpus":
-        osModule.cpus().forEach(({ model, speed }, idx) => {
-          console.log(`CPU: ${coloringItem("yellow", `${idx + 1}`)}`);
-          console.log(`Model: ${coloringItem("yellow", `${model}`)}`);
-          console.log(`Speed: ${coloringItem("yellow", `${speed}\n`)}`);
-        });
-        console.log(
-          `Total CPUs: ${coloringItem("yellow", `${osModule.cpus().length}`)}`
-        );
-        break;
-
-      case "--homedir":
-        console.log(osModule.homedir());
-        break;
-
-      case "--username":
-        console.log(osModule.userInfo().username);
-        break;
-
-      case "--architecture":
-        console.log(osModule.arch());
+        console.log(coloringItem("yellow", JSON.stringify(osModule.EOL)));
         break;
 
       default:
