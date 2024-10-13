48a9bd85f143ca45094f7a4a3ea0416c0808c990
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2024-10-13 15:25:41 +0500

    feat: add username case in os task

--- src/tasks/os.js
+++ src/tasks/os.js
@@ -22,6 +22,10 @@ export const os = (parameter) => {
         console.log(coloringItem("yellow", osModule.homedir()));
         break;
 
+      case "--username":
+        console.log(coloringItem("yellow", osModule.userInfo().username));
+        break;
+
       default:
         console.log(`\n${coloringItem("red", "Invalid input")}`);
     }
