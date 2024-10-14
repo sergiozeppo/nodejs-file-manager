721e675d1a59b13f8db4e61b29f054b361aaa63a
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2024-10-14 21:06:56 +0500

    fix: fix os to operate several arguments at once

--- src/tasks/os.js
+++ src/tasks/os.js
@@ -1,39 +1,41 @@
 import { coloringItem } from "../utils/coloringItem.js";
 import { divisor } from "../constants/constants.js";
 
-export const os = (parameter) => {
+export const os = (...parameters) => {
   import("node:os").then((osModule) => {
-    switch (parameter) {
-      case "--EOL":
-        console.log(coloringItem("yellow", JSON.stringify(osModule.EOL)));
-        break;
+    parameters.forEach((parameter) => {
+      switch (parameter) {
+        case "--EOL":
+          console.log(coloringItem("yellow", JSON.stringify(osModule.EOL)));
+          break;
 
-      case "--cpus":
-        osModule.cpus().forEach(({ model, speed }, idx) => {
-          const ghz = (speed / divisor).toFixed(1);
-          console.log(`CPU: ${coloringItem("yellow", `${idx + 1}`)}`);
-          console.log(`Model: ${coloringItem("yellow", `${model}`)}`);
-          console.log(`Speed: ${coloringItem("yellow", `${ghz} Ghz\n`)}`);
-        });
-        console.log(
-          `Total CPUs: ${coloringItem("yellow", `${osModule.cpus().length}`)}`
-        );
-        break;
+        case "--cpus":
+          osModule.cpus().forEach(({ model, speed }, idx) => {
+            const ghz = (speed / divisor).toFixed(1);
+            console.log(`CPU: ${coloringItem("yellow", `${idx + 1}`)}`);
+            console.log(`Model: ${coloringItem("yellow", `${model}`)}`);
+            console.log(`Speed: ${coloringItem("yellow", `${ghz} Ghz\n`)}`);
+          });
+          console.log(
+            `Total CPUs: ${coloringItem("yellow", `${osModule.cpus().length}`)}`
+          );
+          break;
 
-      case "--homedir":
-        console.log(coloringItem("yellow", osModule.homedir()));
-        break;
+        case "--homedir":
+          console.log(coloringItem("yellow", osModule.homedir()));
+          break;
 
-      case "--username":
-        console.log(coloringItem("yellow", osModule.userInfo().username));
-        break;
+        case "--username":
+          console.log(coloringItem("yellow", osModule.userInfo().username));
+          break;
 
-      case "--architecture":
-        console.log(coloringItem("yellow", osModule.arch()));
-        break;
+        case "--architecture":
+          console.log(coloringItem("yellow", osModule.arch()));
+          break;
 
-      default:
-        console.log(`${coloringItem("red", "Invalid input")}`);
-    }
+        default:
+          console.log(`${coloringItem("red", "Invalid input")}`);
+      }
+    });
   });
 };
